9ee7f08151b3536ea605b618b04dad0bd4ae1640
AlexZ <alexisboianelli1234@gmail.com>
2023-06-26 21:29:23 -0300

    update 4.1

--- client/script.js
+++ client/script.js
@@ -1,10 +1,10 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
-const API_KEY = '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';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
+
 
 let loadInterval;
 
