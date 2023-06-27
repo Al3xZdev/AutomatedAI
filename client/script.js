dc15b84126e30a73db7d948020cd12538f7b0238
AlexZ <alexisboianelli1234@gmail.com>
2023-06-26 21:13:55 -0300

    update 3

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
-const axios = require('axios');
+
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
